34349a62a526c819b83ba38c76b469b1d7848019
nnikitochka <asopiasoska@gmail.com>
2025-07-08 06:21:43 +0700

    Поле artists класса Album теперь хранит List<ArtistMeta>, вместо List<Artist> ввиду некоторых ограничений api

--- ymd-api/src/main/kotlin/ru/nnedition/ymdownloader/api/objects/album/Album.kt
+++ ymd-api/src/main/kotlin/ru/nnedition/ymdownloader/api/objects/album/Album.kt
@@ -1,8 +1,8 @@
 package ru.nnedition.ymdownloader.api.objects.album
 
 import com.google.gson.annotations.SerializedName
-import ru.nnedition.ymdownloader.api.objects.artist.Artist
 import ru.nnedition.ymdownloader.api.objects.Track
+import ru.nnedition.ymdownloader.api.objects.artist.ArtistMeta
 
 data class Album(
     val id: Long,
@@ -10,7 +10,7 @@ data class Album(
     val year: Int,
     val coverUri: String,
     val genre: String = "none",
-    val artists: List<Artist>,
+    val artists: List<ArtistMeta>,
     val available: Boolean,
     @SerializedName("volumes")
     val tracks: List<List<Track>>,
