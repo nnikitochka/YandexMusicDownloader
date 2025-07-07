3e46e19ecd183aa419c3515b84ee134738780ed9
nnikitochka <asopiasoska@gmail.com>
2025-07-08 04:40:34 +0700

    Попытка исправления публикации артефакта #4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,35 @@
 plugins {
     kotlin("jvm") version "2.1.20"
+    id("maven-publish")
 }
 
 allprojects {
-    apply(plugin = "org.jetbrains.kotlin.jvm")
-
     group = "ru.nnedition.ymdownloader"
     version = "0.2.2"
 
     repositories {
-        mavenCentral()
         maven("https://jitpack.io")
+        mavenCentral()
     }
+}
+
+subprojects {
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "maven-publish")
 
     kotlin {
         jvmToolchain(21)
     }
 
+    java {
+        withSourcesJar()
+    }
+
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                from(components["java"])
+            }
+        }
+    }
 }
