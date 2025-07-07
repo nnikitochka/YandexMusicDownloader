f21391fcccc9ee9a82c76beac918a80da12812e9
nnikitochka <asopiasoska@gmail.com>
2025-07-08 04:17:03 +0700

    Попытка исправления публикации артефакта #2

--- ymd-console-client/build.gradle.kts
+++ ymd-console-client/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.gradleup.shadow") version "8.3.0"
-    `maven-publish`
 }
 
 group = "ru.nnedition.ymdownloader"
@@ -8,21 +7,13 @@ version = "1.1"
 
 dependencies {
     implementation(project(":ymd-api"))
-
     implementation("com.moandjiezana.toml:toml4j:0.7.2")
-
     implementation("org.slf4j:slf4j-api:2.0.17")
     implementation("com.github.nnikitochka:YetAnotherLogger:1.1")
 }
 
 tasks {
     jar {
-        manifest {
-            attributes["Main-Class"] = "ru.nnedition.ymdownloader.Main"
-            attributes["Implementation-Title"] = "YandexMusicDownloader"
-            attributes["Implementation-Version"] = version
-        }
-
         enabled = false
     }
 
@@ -34,31 +25,11 @@ tasks {
         mergeServiceFiles()
         archiveClassifier.set("")
         archiveFileName.set("YandexMusicDownloader.jar")
-    }
-}
 
-// Конфигурация публикации
-publishing {
-    publications {
-        create<MavenPublication>("ymd-console-client") {
-            artifact(tasks["shadowJar"])
-
-            pom {
-                name.set("Yandex Music Downloader")
-                url.set("https://github.com/nnikitochka/YandexMusicDownloader")
-
-                withXml {
-                    val dependenciesNode = asNode().appendNode("dependencies")
-                    configurations.implementation.get().allDependencies.forEach {
-                        if (it.group != null && it.name != "unspecified") {
-                            val dependencyNode = dependenciesNode.appendNode("dependency")
-                            dependencyNode.appendNode("groupId", it.group)
-                            dependencyNode.appendNode("artifactId", it.name)
-                            dependencyNode.appendNode("version", it.version)
-                        }
-                    }
-                }
-            }
+        manifest {
+            attributes["Main-Class"] = "ru.nnedition.ymdownloader.Main"
+            attributes["Implementation-Title"] = "YandexMusicDownloader"
+            attributes["Implementation-Version"] = version
         }
     }
 }
