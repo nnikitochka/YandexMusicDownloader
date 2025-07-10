582eb7e53069cdc7ef476e5f1631e1008571b30a
nnikitochka <asopiasoska@gmail.com>
2025-07-10 20:08:17 +0700

    Добавлена нормальная проверка на исполняемый файл FFmpeg

--- ymd-api/src/main/kotlin/ru/nnedition/ymdownloader/api/ffmpeg/FileFFmpegProvider.kt
+++ ymd-api/src/main/kotlin/ru/nnedition/ymdownloader/api/ffmpeg/FileFFmpegProvider.kt
@@ -3,6 +3,10 @@ package ru.nnedition.ymdownloader.api.ffmpeg
 import java.io.File
 import java.io.IOException
 
+/**
+ * Реализация [FFmpegProvider], которая использует локальный исполняемый файл ffmpeg.
+ * @param ffmpegFile Файл исполняемого файла ffmpeg.
+ */
 open class FileFFmpegProvider(
     val ffmpegFile: File
 ) : FFmpegProvider {
@@ -11,9 +15,12 @@ open class FileFFmpegProvider(
     ) : this(File(ffmpegPath))
 
     init {
-        require(ffmpegFile.exists()) { "FFmpeg не был найден по пути: ${ffmpegFile.path}." }
+        require(isFFmpeg()) { "FFmpeg не был найден по пути: ${ffmpegFile.path}." }
     }
 
+    /**
+     * @throws IOException Если произошла ошибка при выполнении ffmpeg.
+     */
     override fun mux(inPath: File, outPath: File) {
         val process = ProcessBuilder(
             ffmpegFile.absolutePath,
@@ -29,6 +36,25 @@ open class FileFFmpegProvider(
         if (exitCode != 0) {
             val errorOutput = process.errorStream.bufferedReader().readText()
             throw IOException("ffmpeg failed: $errorOutput")
+        }
+    }
+
+    /**
+     * Проверяет, является ли указанный файл ffmpeg исполняемым и корректным.
+     * @return true, если файл является корректным ffmpeg, иначе false.
+     */
+    fun isFFmpeg(): Boolean {
+        if (!ffmpegFile.exists() || !ffmpegFile.canExecute()) return false
+
+        return try {
+            val process = Runtime.getRuntime().exec(arrayOf(ffmpegFile.path, "-version"))
+            val reader = process.inputStream.bufferedReader()
+            val output = reader.readText()
+            reader.close()
+
+            process.waitFor() == 0 && output.contains("ffmpeg version")
+        } catch (e: Exception) {
+            false
         }
     }
 }
