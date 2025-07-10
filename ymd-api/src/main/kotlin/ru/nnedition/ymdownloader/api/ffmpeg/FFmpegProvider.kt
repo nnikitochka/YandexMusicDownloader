4d98da22602a73633e740f4331266cd46d29a169
nnikitochka <asopiasoska@gmail.com>
2025-07-10 20:07:33 +0700

    Добавлены разъясняющие комментарии

--- ymd-api/src/main/kotlin/ru/nnedition/ymdownloader/api/ffmpeg/FFmpegProvider.kt
+++ ymd-api/src/main/kotlin/ru/nnedition/ymdownloader/api/ffmpeg/FFmpegProvider.kt
@@ -2,6 +2,15 @@ package ru.nnedition.ymdownloader.api.ffmpeg
 
 import java.io.File
 
+/**
+ * Интерфейс для работы с ffmpeg.
+ * Предоставляет методы для выполнения mux (перекодирования) аудиофайлов.
+ */
 interface FFmpegProvider {
+    /**
+     * Выполняет mux (перекодирование) аудиофайла с использованием ffmpeg.
+     * @param inPath Путь к входному аудиофайлу.
+     * @param outPath Путь к выходному аудиофайлу.
+     */
     fun mux(inPath: File, outPath: File)
 }
