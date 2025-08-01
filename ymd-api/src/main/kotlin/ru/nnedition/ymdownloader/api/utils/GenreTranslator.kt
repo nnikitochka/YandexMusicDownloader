ee3e17618707ab8e4413d12817f115490090f703
nnikitochka <asopiasoska@gmail.com>
2025-08-01 07:20:25 +0700

    Жанры были вынесены в файл

--- ymd-api/src/main/kotlin/ru/nnedition/ymdownloader/api/utils/GenreTranslator.kt
+++ ymd-api/src/main/kotlin/ru/nnedition/ymdownloader/api/utils/GenreTranslator.kt
@@ -1,28 +1,49 @@
 package ru.nnedition.ymdownloader.api.utils
 
-object GenreTranslator {
-    private val genres = hashMapOf(
-        "alternative" to "Альтернатива",
-        "pop" to "Поп",
-        "ruspop" to "Русский поп",
-        "rock" to "Рок",
-        "rusrock" to "Русский рок",
-        "metal" to "Метал",
-        "jazz" to "Джаз",
-        "hip-hop" to "Хип-хоп",
-        "electronic" to "Электронная музыка",
-        "classical" to "Классика",
-        "country" to "Кантри",
-        "blues" to "Блюз",
-        "videogame" to "Видеоигры",
-        "classicmetal" to "Классический метал",
-        "rap" to "Рэп",
-        "allrock" to "Рок",
-        "punk" to "Панк рок",
-        "folkmetal" to "Фолк-метал",
-        "industrial" to "Индастриал",
-        "folkrock" to "Фолк-рок",
-    )
+import com.google.gson.Gson
+import java.io.File
 
-    fun translate(genre: String) = genres[genre]
+object GenreTranslator {
+    private val gson = Gson()
+
+    var translationsFile = File("genres.json")
+    private val genres: MutableMap<String, String> = HashMap()
+
+    fun loadTranslations() {
+        if (!this.translationsFile.exists()) {
+            val input = this.javaClass.classLoader.getResourceAsStream("genres.json")
+
+            require(input != null) { "genres.json не найден!" }
+
+            this.translationsFile.writeBytes(input.readBytes())
+        }
+
+        val translations = gson.fromJson(this.translationsFile.readText(), Map::class.java)
+            ?.map { it.key.toString() to it.value.toString() }?.toMap()
+
+        require(translations != null) { "Ошибка чтения файла!" }
+
+        this.genres.putAll(translations)
+    }
+
+    fun foldTranslations() {
+        if (!this.translationsFile.exists()) {
+            val input = this.javaClass.classLoader.getResourceAsStream("genres.json")
+
+            require(input != null) { "genres.json не найден!" }
+
+            this.translationsFile.writeBytes(input.readBytes())
+            return
+        }
+
+        val json = this.gson.toJson(this.genres)
+
+        this.translationsFile.writeText(json)
+    }
+
+    fun saveTranslation(translation: Pair<String, String>) {
+        genres.put(translation.first, translation.second)
+    }
+
+    fun translate(genre: String) = this.genres[genre]
 }
